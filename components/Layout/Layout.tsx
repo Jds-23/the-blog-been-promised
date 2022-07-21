ca92b05d3964de0904895fcfe57f390ab691f459
Joydeep Singha <joydeepsingha68@gmail.com>
2022-07-21 18:53:04 +0530

    style: make bg fixed

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -8,7 +8,7 @@ const Layout: React.FC<Props> = ({ children }) => {
   return (
     <div className="w-full px-4 relative z-0 min-h-screen">
       <Header />
-      <div className="top-0 left-0 absolute w-full min-h-screen bg-bg-main bg-no-repeat bg-cover bg-center" />
+      <div className="top-0 left-0 fixed w-full min-h-screen bg-bg-main bg-no-repeat bg-cover bg-center" />
       {children}
     </div>
   );
